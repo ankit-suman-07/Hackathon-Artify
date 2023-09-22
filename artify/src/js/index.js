2fe1503d50de8df74fbd9fe0d68de8677d8db6e9
ankit-suman-07 <ankitsuman07@gmail.com>
2023-09-23 01:30:05 +0530

    Added unsplash api

--- artify/src/js/index.js
+++ artify/src/js/index.js
@@ -1,3 +1,26 @@
-
+import { createApi } from 'unsplash-js';
 
 const artGallery = document.querySelector('.art-gallery');
+const unsplash = createApi({
+  accessKey: 'NJnPX52l0KU_-gIWlM7F3Bf7XnnxFI-BvOquBZqMP0g',
+});
+
+unsplash.search.getPhotos({
+  query: 'Art',
+  page: 1,
+  perPage: 12,
+  orientation: 'squarish',
+  orderBy: 'latest'
+}).then(result => {
+  if (result.type === 'success') {
+    const photos = result.response.results;
+    console.log(photos);
+    const getUrls = photos.map((i) => {
+      return(
+      `<div class="art" >
+        <img src="${i.urls.regular}" />
+      </div>`);
+    })
+    artGallery.innerHTML = getUrls.join('');
+  }
+})
